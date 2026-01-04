e27e12564618e81cf0934faf85908967143eb68c
Vu <vuanhle10@gmail.com>
2026-01-03 21:22:16 -0800

    feat: Add SIMD acceleration and std library modules
    stdlib.c additions:
    - SIMD Vec4f struct with SSE intrinsics support
    - Vector ops: add, sub, mul, dot, magnitude, normalize, cross
    - SIMD-accelerated tensor add/mul (4x throughput with SSE)
    
    New std library modules:
    - std/simd.mn: Vec4f, Mat4, vector operations
    - std/flow.mn: Channel, Pipeline, Stream for data-flow
    - std/alloc.mn: Arena allocator, Pool allocator
    
    Implements roadmap Milestone 5 (std library expansion)

--- compilers/shared/runtime/stdlib.c
+++ compilers/shared/runtime/stdlib.c
@@ -186,77 +186,205 @@ double gul_ml_relu(double x) { return x > 0.0 ? x : 0.0; }
 
 // Allocate tensor data (flat array)
 int64_t gul_tensor_alloc(int64_t num_elements) {
-    double *data = (double *)malloc(num_elements * sizeof(double));
-    if (!data) {
-        fprintf(stderr, "GUL Runtime: Out of memory allocating tensor\n");
-        exit(1);
-    }
-    return (int64_t)data;
+  double *data = (double *)malloc(num_elements * sizeof(double));
+  if (!data) {
+    fprintf(stderr, "GUL Runtime: Out of memory allocating tensor\n");
+    exit(1);
+  }
+  return (int64_t)data;
 }
 
 // Free tensor data
-void gul_tensor_free(int64_t ptr) {
-    free((void *)ptr);
-}
+void gul_tensor_free(int64_t ptr) { free((void *)ptr); }
 
 // Fill tensor with value
 void gul_tensor_fill(int64_t ptr, int64_t size, double value) {
-    double *data = (double *)ptr;
-    for (int64_t i = 0; i < size; i++) {
-        data[i] = value;
-    }
+  double *data = (double *)ptr;
+  for (int64_t i = 0; i < size; i++) {
+    data[i] = value;
+  }
 }
 
 // Element-wise add
 void gul_tensor_add(int64_t dst, int64_t a, int64_t b, int64_t size) {
-    double *d = (double *)dst;
-    double *x = (double *)a;
-    double *y = (double *)b;
-    for (int64_t i = 0; i < size; i++) {
-        d[i] = x[i] + y[i];
-    }
+  double *d = (double *)dst;
+  double *x = (double *)a;
+  double *y = (double *)b;
+  for (int64_t i = 0; i < size; i++) {
+    d[i] = x[i] + y[i];
+  }
 }
 
 // Element-wise multiply
 void gul_tensor_mul(int64_t dst, int64_t a, int64_t b, int64_t size) {
-    double *d = (double *)dst;
-    double *x = (double *)a;
-    double *y = (double *)b;
-    for (int64_t i = 0; i < size; i++) {
-        d[i] = x[i] * y[i];
-    }
+  double *d = (double *)dst;
+  double *x = (double *)a;
+  double *y = (double *)b;
+  for (int64_t i = 0; i < size; i++) {
+    d[i] = x[i] * y[i];
+  }
 }
 
 // Matrix multiply: C[m,n] = A[m,k] * B[k,n]
-void gul_tensor_matmul(int64_t c_ptr, int64_t a_ptr, int64_t b_ptr,
-                        int64_t m, int64_t k, int64_t n) {
-    double *C = (double *)c_ptr;
-    double *A = (double *)a_ptr;
-    double *B = (double *)b_ptr;
-    
-    for (int64_t i = 0; i < m; i++) {
-        for (int64_t j = 0; j < n; j++) {
-            double sum = 0.0;
-            for (int64_t l = 0; l < k; l++) {
-                sum += A[i * k + l] * B[l * n + j];
-            }
-            C[i * n + j] = sum;
-        }
+void gul_tensor_matmul(int64_t c_ptr, int64_t a_ptr, int64_t b_ptr, int64_t m,
+                       int64_t k, int64_t n) {
+  double *C = (double *)c_ptr;
+  double *A = (double *)a_ptr;
+  double *B = (double *)b_ptr;
+
+  for (int64_t i = 0; i < m; i++) {
+    for (int64_t j = 0; j < n; j++) {
+      double sum = 0.0;
+      for (int64_t l = 0; l < k; l++) {
+        sum += A[i * k + l] * B[l * n + j];
+      }
+      C[i * n + j] = sum;
     }
+  }
 }
 
 // Sum all elements
 double gul_tensor_sum(int64_t ptr, int64_t size) {
-    double *data = (double *)ptr;
-    double sum = 0.0;
-    for (int64_t i = 0; i < size; i++) {
-        sum += data[i];
-    }
-    return sum;
+  double *data = (double *)ptr;
+  double sum = 0.0;
+  for (int64_t i = 0; i < size; i++) {
+    sum += data[i];
+  }
+  return sum;
 }
 
 // Mean of all elements
 double gul_tensor_mean(int64_t ptr, int64_t size) {
-    return gul_tensor_sum(ptr, size) / (double)size;
+  return gul_tensor_sum(ptr, size) / (double)size;
 }
 
+// ============================================================================
+// SIMD Vector Operations (4-element float vectors)
+// ============================================================================
+
+#ifdef __SSE__
+#include <xmmintrin.h>
+#define GUL_HAS_SSE 1
+#else
+#define GUL_HAS_SSE 0
+#endif
+
+// Vec4f: 4 x float32 vector
+typedef struct {
+  float x, y, z, w;
+} GulVec4f;
+
+// Create vector
+GulVec4f gul_simd_vec4f(float x, float y, float z, float w) {
+  return (GulVec4f){x, y, z, w};
+}
+
+// Vector addition
+GulVec4f gul_simd_add(GulVec4f a, GulVec4f b) {
+#if GUL_HAS_SSE
+  __m128 va = _mm_set_ps(a.w, a.z, a.y, a.x);
+  __m128 vb = _mm_set_ps(b.w, b.z, b.y, b.x);
+  __m128 vr = _mm_add_ps(va, vb);
+  float r[4];
+  _mm_storeu_ps(r, vr);
+  return (GulVec4f){r[0], r[1], r[2], r[3]};
+#else
+  return (GulVec4f){a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w};
+#endif
+}
+
+// Vector subtraction
+GulVec4f gul_simd_sub(GulVec4f a, GulVec4f b) {
+#if GUL_HAS_SSE
+  __m128 va = _mm_set_ps(a.w, a.z, a.y, a.x);
+  __m128 vb = _mm_set_ps(b.w, b.z, b.y, b.x);
+  __m128 vr = _mm_sub_ps(va, vb);
+  float r[4];
+  _mm_storeu_ps(r, vr);
+  return (GulVec4f){r[0], r[1], r[2], r[3]};
+#else
+  return (GulVec4f){a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w};
+#endif
+}
+
+// Vector multiply (element-wise)
+GulVec4f gul_simd_mul(GulVec4f a, GulVec4f b) {
+#if GUL_HAS_SSE
+  __m128 va = _mm_set_ps(a.w, a.z, a.y, a.x);
+  __m128 vb = _mm_set_ps(b.w, b.z, b.y, b.x);
+  __m128 vr = _mm_mul_ps(va, vb);
+  float r[4];
+  _mm_storeu_ps(r, vr);
+  return (GulVec4f){r[0], r[1], r[2], r[3]};
+#else
+  return (GulVec4f){a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w};
+#endif
+}
+
+// Dot product
+float gul_simd_dot(GulVec4f a, GulVec4f b) {
+  GulVec4f m = gul_simd_mul(a, b);
+  return m.x + m.y + m.z + m.w;
+}
+
+// Magnitude (length)
+float gul_simd_magnitude(GulVec4f v) { return sqrtf(gul_simd_dot(v, v)); }
+
+// Normalize
+GulVec4f gul_simd_normalize(GulVec4f v) {
+  float mag = gul_simd_magnitude(v);
+  if (mag > 0.0f) {
+    return (GulVec4f){v.x / mag, v.y / mag, v.z / mag, v.w / mag};
+  }
+  return v;
+}
+
+// Cross product (3D, ignores w)
+GulVec4f gul_simd_cross(GulVec4f a, GulVec4f b) {
+  return (GulVec4f){a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z,
+                    a.x * b.y - a.y * b.x, 0.0f};
+}
+
+// ============================================================================
+// SIMD-accelerated tensor operations (process 4 elements at a time)
+// ============================================================================
+
+void gul_tensor_add_simd(int64_t dst, int64_t a, int64_t b, int64_t size) {
+  float *d = (float *)dst;
+  float *x = (float *)a;
+  float *y = (float *)b;
+
+  int64_t i = 0;
+#if GUL_HAS_SSE
+  // Process 4 floats at a time
+  for (; i + 4 <= size; i += 4) {
+    __m128 va = _mm_loadu_ps(x + i);
+    __m128 vb = _mm_loadu_ps(y + i);
+    __m128 vr = _mm_add_ps(va, vb);
+    _mm_storeu_ps(d + i, vr);
+  }
+#endif
+  // Handle remaining elements
+  for (; i < size; i++) {
+    d[i] = x[i] + y[i];
+  }
+}
+
+void gul_tensor_mul_simd(int64_t dst, int64_t a, int64_t b, int64_t size) {
+  float *d = (float *)dst;
+  float *x = (float *)a;
+  float *y = (float *)b;
+
+  int64_t i = 0;
+#if GUL_HAS_SSE
+  for (; i + 4 <= size; i += 4) {
+    __m128 va = _mm_loadu_ps(x + i);
+    __m128 vb = _mm_loadu_ps(y + i);
+    __m128 vr = _mm_mul_ps(va, vb);
+    _mm_storeu_ps(d + i, vr);
+  }
+#endif
+  for (; i < size; i++) {
+    d[i] = x[i] * y[i];
+  }
+}
